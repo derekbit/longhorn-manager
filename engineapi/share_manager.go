5217ee1d62e966ecfe87ac188681c03876a99511
Derek Su <derek.su@suse.com>
2023-03-18 23:58:40 +0800

    engineapi: Introduce gRPC APIs for share manager
    Longhorn 5143
    
    Signed-off-by: Derek Su <derek.su@suse.com>

---
+++ engineapi/share_manager.go
@@ -0,0 +1,27 @@
+package engineapi
+
+import (
+	"fmt"
+
+	longhorn "github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2"
+	smclient "github.com/longhorn/longhorn-share-manager/pkg/client"
+	v1 "k8s.io/api/core/v1"
+)
+
+type ShareManagerClient struct {
+	grpcClient *smclient.ShareManagerClient
+}
+
+func NewShareManagerClient(sm *longhorn.ShareManager, pod *v1.Pod) (*ShareManagerClient, error) {
+	if sm.Status.State != longhorn.ShareManagerStateRunning {
+		return nil, fmt.Errorf("invalid Share Manager %v, state: %v", sm.Name, sm.Status.State)
+	}
+
+	return &ShareManagerClient{
+		grpcClient: smclient.NewShareManagerClient(fmt.Sprintf("%s:%d", pod.Status.PodIP, ShareManagerDefaultPort)),
+	}, nil
+}
+
+func (c *ShareManagerClient) FilesystemTrim(isEncryptedDevice bool) error {
+	return c.grpcClient.FilesystemTrim(isEncryptedDevice)
+}
