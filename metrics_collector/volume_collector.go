49dc283b83b285403cbafb7aa487b704eda62aea
Derek Su <derek.su@suse.com>
2026-03-02 04:55:37 +0000

    fix(metrics): fix potential NPE
    Longhorn 12731
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- metrics_collector/volume_collector.go
+++ metrics_collector/volume_collector.go
@@ -243,6 +243,11 @@ func (vc *VolumeCollector) collectMetrics(ch chan<- prometheus.Metric, v *longho
 		return
 	}
 
+	if e == nil {
+		vc.logger.Debugf("Volume %v does not have current engine, skip collecting performance metrics", v.Name)
+		return
+	}
+
 	engineClientProxy, err := vc.getEngineClientProxy(e)
 	if err != nil {
 		vc.logger.WithError(err).Debugf("Failed to get engine proxy of %v for volume %v", e.Name, v.Name)
